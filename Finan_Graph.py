12300f204d78569b2c6df0ffc2ea747a9820fcb0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-08-30 17:18:21 +0200

    Update Finan_Graph.py

--- Finan_Graph.py
+++ Finan_Graph.py
@@ -1,7 +1,7 @@
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 from matplotlib import style
-import datetime
+from datetime import datetime, timedelta
 from VALID import ns
 
 #estilos = plt.style.available
@@ -9,18 +9,23 @@ from VALID import ns
 
 #style.use('ggplot')
 
+fecha_partida = datetime.now() - timedelta(days = 30)
+
 while True:
     co=input("Comp: ")
-    comp = pdr.get_data_yahoo(co,start=datetime.datetime(2019,1,1))
+    
+    try:
+        comp = pdr.get_data_yahoo(co,start= fecha_partida)
+        print(comp)
 
-    print(comp)
-
-    datos = comp['Adj Close']
-
-    datos.plot(grid=True)
-    plt.show()
+        datos = comp['Adj Close']
+        datos.plot(grid=True)
+        plt.show()
+    except:
+        print("HUBO UN PROBLEMA AL ESTABLECER LA CONEXION")
 
     conti=ns(input("¿Continuar?: "))
     if conti == "n":
         break
+    
     
