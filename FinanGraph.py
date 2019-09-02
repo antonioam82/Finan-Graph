82ad48883dd5f352cbc972b2c90199e6f4c85808
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-09-02 13:02:04 +0200

    Update FinanGraph.py

--- FinanGraph.py
+++ FinanGraph.py
@@ -13,7 +13,17 @@ from datetime import datetime, timedelta
 from math import *
 
 ventana = tkinter.Tk()
-ventana.wm_title("Precio acciones")
+ventana.wm_title("Evolucion mensual, Precio acciones")
 espacio=ventana.geometry("1000x700")
+
+fig = Figure()
+
+canvas = FigureCanvasTkAgg(fig, master=ventana)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+
+
 
 tkinter.mainloop()
