cae091dd1311fe100371e2bff26c5284dec5c6bd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-09-08 20:13:00 +0200

    Update finan_graph.py

--- finan_graph.py
+++ finan_graph.py
@@ -2,25 +2,26 @@ import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 from matplotlib import style
 from datetime import datetime, timedelta
-from VALID import ns
+from VALID import ns, OKI
 
 #estilos = plt.style.available
 #print(estilos)
 
-#style.use('ggplot')
-
-fecha_partida = datetime.now() - timedelta(days = 90)
+style.use('dark_background')
 
 while True:
     co=input("Comp: ")
     param=input("Param: ")
+    period=OKI(input("Periodo en días: "))
+
+    fecha_partida = datetime.now() - timedelta(days = period)
     
     try:
         comp = pdr.get_data_yahoo(co,start= fecha_partida)
         print(comp)
 
         datos = comp[param]
-        datos.plot(grid=True,figsize=(15,7))
+        datos.plot(grid=True,figsize=(13,7))
         plt.title("Datos "+co)
         plt.show()
     except:
