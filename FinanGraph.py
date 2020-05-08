404cc27392399d189010b32ce374e0739840c1fd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-08 17:03:35 +0200

    Update FinanGraph.py

--- FinanGraph.py
+++ FinanGraph.py
@@ -6,7 +6,7 @@ import numpy as np
 
 ventana = tkinter.Tk()
 ventana.title("Finan Graph")
-ventana.geometry("1000x700")
+ventana.geometry("1040x700")
 
 fig = Figure()
 ax1 = fig.add_subplot(111)
@@ -15,7 +15,10 @@ canvas = FigureCanvasTkAgg(fig, master=ventana)
 canvas.draw()
 canvas.get_tk_widget().pack(side=tkinter.BOTTOM,fill=tkinter.BOTH, expand=1)
 
-entry = tkinter.Entry(master=ventana,width=30)
+labelSym = tkinter.Label(master=ventana,text="Symbol",width=8)
+labelSym.pack(side=tkinter.LEFT)
+
+entry = tkinter.Entry(master=ventana,width=8)
 entry.pack(side=tkinter.LEFT)
 
 plt.show()
