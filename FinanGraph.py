97e16bd318a0eb09d3d94a641bd8d585883633c0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-10 14:17:55 +0200

    Update FinanGraph.py

--- FinanGraph.py
+++ FinanGraph.py
@@ -4,12 +4,9 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import matplotlib.animation as animation
 import numpy as np
 
-def get_info():
-    init_date = datetime.now() - timedelta(days = int(entry3.get()))
-    info = pdr.get_data_yahoo(entry.get(),start = init_date)
-    #print(info)
 
 ventana = Tk()
 ventana.title("Finan Graph")
@@ -17,17 +14,41 @@ ventana.geometry("1040x700")
 ventana.configure(background="light blue")
 symbol_entry = StringVar()
 time_range = IntVar()
+actv = False
+info = ""
 
 fig = Figure()
 ax1 = fig.add_subplot(111)
 ax1.grid()
 
-canvas = FigureCanvasTkAgg(fig, master=ventana)
+
+canvas = FigureCanvasTkAgg(fig,master=ventana)
 canvas.draw()
 
-toolbar = NavigationToolbar2Tk(canvas, ventana)# barra de iconos
+toolbar = NavigationToolbar2Tk(canvas, ventana)
 toolbar.update()
 canvas.get_tk_widget().pack(side=BOTTOM,fill=BOTH, expand=1)
+
+def activate():
+    global actv
+    actv = True
+
+def get_info():
+    global actv
+    ax1.clear()
+    ax1.grid()
+    init_date = datetime.now() - timedelta(days = int(entry3.get()))
+    info = pdr.get_data_yahoo(entry.get(),start = init_date)
+    ax1.plot(info)
+    actv = False
+
+def represent(i):
+    global actv
+    if actv == True:
+        get_info()
+    #ani.event_source.start()
+
+ani = animation.FuncAnimation(fig, represent, interval=1000)  
 
 labelSym = Label(master=ventana,bg="light blue",text="Symbol:",width=8,height=2)
 labelSym.pack(side=LEFT)
@@ -41,9 +62,10 @@ labelRange = Label(master=ventana,text="Periodo en días:",bg="light blue",width
 labelRange.place(x=272,y=0)
 entry3 = Entry(master=ventana,width=8,textvariable=time_range)
 entry3.place(x=369,y=8)
-graph = Button(master=ventana,text="VIEW GRAPH",command=get_info,height=1)
+graph = Button(master=ventana,text="VIEW GRAPH",command=activate,height=1)
 graph.place(x=958,y=6)
 
-ventana.mainloop()
+plt.show()
 
+ventana.mainloop()
 
