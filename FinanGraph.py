e06ae1caeeeeb29bc281e089f7e91e470c07ca46
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-26 17:24:27 +0200

    Update FinanGraph.py

--- FinanGraph.py
+++ FinanGraph.py
@@ -1,5 +1,4 @@
 import pandas_datareader as pdr
-from alpha_vantage.techindicators import TechIndicators
 import pickle
 from tkinter import *
 from tkinter import ttk
@@ -74,7 +73,6 @@ def get_info():
                 print(i)
                 ax1.plot(info[i])
             ax1.legend((datas),loc='upper right', shadow=False)
-            print("OK")
 
             if not entry.get() in used_symbols:
                 used_symbols.insert(0,entry.get())
@@ -101,7 +99,6 @@ def represent(i):
     global actv   
     if actv == True:
         get_info()
-    #ani.event_source.start()
 
 ani = animation.FuncAnimation(fig, represent, interval=1000)  
 
@@ -112,10 +109,6 @@ entry = ttk.Combobox(master=ventana,width=8)
        #"^IBEX","^IXIC","^N225","BTC-EUR"]
 entry["values"]=used_symbols
 entry.pack(side=LEFT)
-#labelCom = Label(master=ventana,bg="light blue",text="Compare with:",width=10,height=2)
-#labelCom.place(x=125,y=0)
-#entry2 = Entry(master=ventana,width=8)
-#entry2.place(x=210,y=8)
 labelRange = Label(master=ventana,text="Time (days):",bg="light blue",width=13,height=2)
 labelRange.place(x=135,y=0)
 entry3 = Entry(master=ventana,width=8,textvariable=time_range)
@@ -137,8 +130,6 @@ btnC.place(x=441,y=5)
 
 item_list=["High","Low","Open","Close"]
 buttons = {"High":btnH,"Low":btnL,"Open":btnV,"Close":btnC}
-
-#plt.show()
 
 ventana.mainloop()
 
