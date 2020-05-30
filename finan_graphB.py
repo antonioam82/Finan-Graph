2e5cde4ae8afbf7d326d48d64590c02adf46650c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-30 15:36:07 +0200

    Update finan_graphB.py

--- finan_graphB.py
+++ finan_graphB.py
@@ -1,3 +1,6 @@
+import pandas_datareader as pdr
+from alpha_vantage.techindicators import TechIndicators
+import pickle
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -36,6 +39,38 @@ class app:
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.ventana)
         self.toolbar.update()
         self.canvas.get_tk_widget().pack(side=BOTTOM,fill=BOTH,expand=1)
+
+        self.labelSym = Label(self.ventana,bg="light blue",text="Symbol:",width=8,height=2)
+        self.labelSym.pack(side=LEFT)
+
+        self.entry = ttk.Combobox(self.ventana,width=8)
+        self.entry["values"]=self.used_symbols
+        self.entry.pack(side=LEFT)
+        self.labelRange = Label(self.ventana,text="Time (days):",bg="light blue",width=13,height=2)
+        self.labelRange.place(x=135,y=0)
+        self.entry3 = Entry(self.ventana,width=8,textvariable=self.time_range)
+        self.entry3.place(x=220,y=8)
+        self.more_info = Button(self.ventana,text="SHOW TABLE",state='disabled')
+        self.more_info.pack(side=RIGHT)
+        self.graph = Button(self.ventana,text="SHOW GRAPH")
+        self.graph.pack(side=RIGHT)
+        self.btnTech = Button(self.ventana,text="BBbands",heigh=1)
+        self.btnTech.place(x=495,y=5)
+        self.labelInfo = Label(self.ventana,text="INFO:",bg="light blue")
+        self.labelInfo.place(x=290,y=8)
+        self.btnH=Button(self.ventana,text="High",bg="gray83")
+        self.btnH.place(x=325,y=5)
+        self.btnL=Button(self.ventana,text="Low",bg="gray83")
+        self.btnL.place(x=364,y=5)
+        self.btnV=Button(self.ventana,text="Open",bg="gray83")
+        self.btnV.place(x=399,y=5)
+        self.btnC=Button(self.ventana,text="Close",bg="light green")
+        self.btnC.place(x=441,y=5)
+
+        self.item_list=["High","Low","Open","Close"]
+        self.buttons = {"High":self.btnH,"Low":self.btnL,"Open":self.btnV,"Close":self.btnC}
+
+
 
         self.ventana.mainloop()
 
