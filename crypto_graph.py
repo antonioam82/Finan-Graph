3b7e5cc933edafb3b445424d7ff027e433a902ec
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-11 19:19:33 +0200

    Create crypto_graph.py

---
+++ crypto_graph.py
@@ -0,0 +1,47 @@
+import pandas_datareader as pdr
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+import pickle
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import tkinter.scrolledtext as sct
+import matplotlib.animation as animation
+from matplotlib import style
+import numpy as np
+
+class app:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Crypto Graph")
+        self.root.geometry("1070x800")
+        self.root.configure(background="light green")
+        self.symbol_entry = StringVar()
+        self.market_entry = StringVar()
+        self.used_symbolsC = pickle.load(open("symbolsC","rb"))
+
+        self.fig = Figure()
+        self.ax1 = self.fig.add_subplot(111)
+        self.ax1.grid()
+
+        self.canvas = FigureCanvasTkAgg(self.fig,master=self.root)
+        self.canvas.draw()
+        
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack(side=BOTTOM,fill=BOTH,expand=1)
+
+        self.labelSym = Label(self.root,bg="light green",text="Symbol:",width=8,height=2)
+        self.labelSym.pack(side=LEFT)
+
+        self.entry = ttk.Combobox(self.root,width=8)
+        self.entry["values"]=self.used_symbolsC
+        self.entry.pack(side=LEFT)        
+        
+
+        self.root.mainloop()
+
+if __name__=="__main__":
+    app()
