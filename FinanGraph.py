407ebd643c0524415cc200f6a5d45032dbca6a03
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-05 12:20:09 +0200

    Update FinanGraph.py

--- FinanGraph.py
+++ FinanGraph.py
@@ -81,7 +81,7 @@ def bands():
 
 def get_info():
     global actv, datas, info, table_head, display_content
-    if selected_items != []:
+    if entry.get() != "" and entry3.get() != "" and int(entry3.get()) > 0:
         try:
             ax1.clear()
             ax1.grid()
@@ -126,14 +126,21 @@ def represent(i):
     global actv   
     if actv == True:
         get_info()
+    #ani.event_source.start()
 
 ani = animation.FuncAnimation(fig, represent, interval=1000)  
 
 labelSym = Label(master=ventana,bg="light blue",text="Symbol:",width=8,height=2)
 labelSym.pack(side=LEFT)
 entry = ttk.Combobox(master=ventana,width=8)
+#entry["values"]=["NFLX","GOGL","AAPL","GM","F","AMZN","BNK","GOLD","FANUY","BTC-EUR","TSLA",
+       #"^IBEX","^IXIC","^N225","BTC-EUR"]
 entry["values"]=used_symbols
 entry.pack(side=LEFT)
+#labelCom = Label(master=ventana,bg="light blue",text="Compare with:",width=10,height=2)
+#labelCom.place(x=125,y=0)
+#entry2 = Entry(master=ventana,width=8)
+#entry2.place(x=210,y=8)
 labelRange = Label(master=ventana,text="Time (days):",bg="light blue",width=13,height=2)
 labelRange.place(x=135,y=0)
 entry3 = Entry(master=ventana,width=8,textvariable=time_range)
@@ -157,6 +164,8 @@ btnC.place(x=441,y=5)
 
 item_list=["High","Low","Open","Close"]
 buttons = {"High":btnH,"Low":btnL,"Open":btnV,"Close":btnC}
+
+#plt.show()
 
 ventana.mainloop()
 
