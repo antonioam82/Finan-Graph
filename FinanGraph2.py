12948ed596430ae8bdfdfccf4f8a0890565ab394
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-06-29 14:30:34 +0200

    Update FinanGraph2.py

--- FinanGraph2.py
+++ FinanGraph2.py
@@ -48,19 +48,19 @@ def make_graph():
     ipc = pdr.get_data_yahoo(tick, start = startdate, end = enddate)
     df = EMA(ipc, 50)
     df2 = EMA(df, 200)
-    df2 = df2[['Close','EMA_50','EMA_200']]
+    df2 = df2[['High','Low','Open','Close','EMA_50','EMA_200']]
     for i in df2:
         ax1.plot(df2[i])
-    ax1.legend(['Close','EMA_50','EMA_200'],loc='best', shadow=False)
+    ax1.legend(['High','Low','Open','Close','EMA_50','EMA_200'],loc='best', shadow=False)
 
 def represent(i):
     global actv
     if actv == True:
         print("activo")
 
-#ani = animation.FuncAnimation(fig, represent, interval=1000)
+ani = animation.FuncAnimation(fig, represent, interval=1000)
 
-#make_graph()
+make_graph()
 
 Label(root,height=2,bg="gray").pack(side=LEFT)
 Label(root,text="TICKER:",bg="gray",fg="white").place(x=10,y=8)
@@ -72,5 +72,17 @@ sts_entry.place(x=240,y=8)
 Label(root,text="END DATE:",bg="gray",fg="white").place(x=316,y=8)
 end_date = Entry(root,textvariable=end_date,width=10)
 end_date.place(x=382,y=8)
+btnHigh = Button(root,text="High",width=5)
+btnHigh.place(x=450,y=5)
+btnLow = Button(root,text="Low",width=5)
+btnLow.place(x=497,y=5)
+btnOpen = Button(root,text="Open",width=5)
+btnOpen.place(x=544,y=5)
+btnClose = Button(root,text="Close",width=5)
+btnClose.place(x=591,y=5)
+btnEMA50 = Button(root,text="EMA 50",width=8)
+btnEMA50.place(x=650,y=5)
+btnEMA200 = Button(root,text="EMA 200",width=8)
+btnEMA200.place(x=716,y=5)
 
 root.mainloop()
