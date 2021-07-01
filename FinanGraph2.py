20340efe0394d78f0ae97ebf72da6c9adba70ea2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-07-01 11:42:58 +0200

    Update FinanGraph2.py

--- FinanGraph2.py
+++ FinanGraph2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from pandas_datareader import data as pdr
+from tkcalendar import *
 import pickle
 from tkinter import *
 from tkinter import ttk
@@ -16,16 +17,33 @@ import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
 
+
+
 def activate():
     global actv
     actv = True
+
+def define_date(n):
+    def new_date(st):
+        if st == 'start':
+            start_date.set(cal.get_date())
+        else:
+            end_date.set(cal.get_date())
+            
+    top = Toplevel()
+    top.title("SET DATE")
+    cal = Calendar(top,selectmode="day",year=2021,month=6,day=22)
+    cal.pack(pady=20)
+    Label(top,text=cal.get_date()).pack(side='bottom')
+    Button(top,text="SET DATE",command=lambda:new_date(n)).pack(side='bottom')
+
+    top.mainloop()
 
 #style.use('dark_background')
 root = Tk()
 root.title("Finan Graph 5")
 root.configure(background="gray")
 root.geometry("1160x800")#1160
-
 start_date = StringVar()
 end_date = StringVar()
 used_symbols = pickle.load(open("symbols","rb"))
@@ -46,12 +64,12 @@ tick_entry.set('IBM')
 tick_entry.place(x=58,y=8)
 Label(root,height=2,bg="gray").pack(side=LEFT)
 Label(root,text="TICKER:",bg="gray",fg="white").place(x=10,y=8)
-Button(root,text="START DATE").place(x=135+11,y=5)
-Button(root,text="END DATE").place(x=316+11,y=5)
+Button(root,text="START DATE",command=lambda:define_date("start")).place(x=135+11,y=5)
+Button(root,text="END DATE",width=8,command=lambda:define_date("end")).place(x=316+11,y=5)
 sts_entry = Entry(root,textvariable=start_date,font=('arial',13),width=10)
 sts_entry.place(x=210+11,y=5)
-end_date = Entry(root,textvariable=end_date,font=('arial',13),width=10)
-end_date.place(x=382+11,y=5)
+end_datee = Entry(root,textvariable=end_date,font=('arial',13),width=10)
+end_datee.place(x=382+11,y=5)
 btnHigh = Button(root,text="High",width=5)
 btnHigh.place(x=450+60,y=5)
 btnLow = Button(root,text="Low",width=5)
@@ -65,6 +83,7 @@ btnEMA50.place(x=650+60,y=5)
 btnEMA200 = Button(root,text="EMA 200",width=8)
 btnEMA200.place(x=776,y=5)
 Button(root,text="SHOW GRAPH",command=activate).place(x=950,y=5)
+    
 
 def EMA(df, n):
     EMA = pd.Series(pd.Series.ewm(df['Close'],span = n, min_periods = n-1, adjust=False).mean(), name='EMA_'+str(n))
@@ -88,8 +107,6 @@ def make_graph():
     ax1.legend(['Close','EMA_50','EMA_200'],loc='best', shadow=False)
     actv = False
     
-    
-
 def represent(i):
     global actv
     if actv == True:
