0cd8869ae065ff3e5255082dfc3ae73faa5bcabd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-01-07 09:35:41 +0100

    Create bollinger_bands.py

---
+++ bollinger_bands.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def get_rolling_mean(values, window):
+    return pd.Series(pd.Series.rolling(values,window).mean(),name='Rolling Mean')
+
+def get_rolling_std(values,window):
+    return pd.Series(pd.Series.rolling(values,window).std(),name='Std Dev')
+
+def get_bollinger_bands(rm,rstd):
+    upper_band = rm + rstd * 5
+    lower_band = rm - rstd * 5
+    return upper_band, lower_band
+
+def test_run():
+    spy = yf.Ticker("SPY")
+    df = spy.history(start='2012-01-01',end='2012-12-31')["Close"]
+
+    rm_SPY = get_rolling_mean(df,window=20)
+    rstd_SPY = get_rolling_std(df,window=20)
+
+    upper_band, lower_band = get_bollinger_bands(rm_SPY,rstd_SPY)
+
+    ax = df.plot(label='SPY')
+    rm_SPY.plot(label='Rolling mean',ax=ax)
+    upper_band.plot(label='upper band',ax=ax)
+    lower_band.plot(label='lower band',ax=ax)
+    plt.grid()
+
+    plt.show()
+
+test_run()
