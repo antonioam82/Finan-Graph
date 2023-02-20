754410c7c2461c1bd4a9df6609d6d4cc65d5ac8b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-02-20 23:20:53 +0100

    Update f-tables2.py

--- f-tables2.py
+++ f-tables2.py
@@ -17,8 +17,8 @@ year = now.year
 head = ""
 tickers = []
 indexed_syms = ['IXIC','N225','GSPC','GDAXI','FCHI','DJI','IBEX','DJT','STOXX50E','FTSE',
-                'HSI','BVSP','MXX','ATX','SSMI','OMXC20','OSEAX','OMXSPI','JKSE',
-                'KS11','TWII','TA125.TA','KLSE','NZ50','CASE30','BVSP','IPSA','JN0U.JO']
+                'HSI','BVSP','MXX','ATX','SSMI','OMXC20','OSEAX','OMXSPI','JKSE','NSEMDCP50',
+                'CRSMID','KS11','TWII','TA125.TA','KLSE','NZ50','CASE30','BVSP','IPSA','JN0U.JO']
 
 def main():
     
