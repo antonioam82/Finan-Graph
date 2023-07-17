587da5a5ef933469c63794a1b28b5fbd82474610
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-07-17 16:11:38 +0200

    Update macd.py

--- macd.py
+++ macd.py
@@ -7,14 +7,16 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 from os import system
-import colorama
+from colorama import Fore, Style, init
 import argparse
+
+init()
 
 def validate_date(d):
     try:
-        datetime.strptime(d, '%Y-%m-%d')
+        return datetime.strptime(d, '%Y-%m-%d')
     except:
-        raise argparse.ArgumentTypeError("Bad date format")
+        raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + "Bad date format (must be '%Y-%m-%d')" + Fore.RESET +Style.RESET_ALL)
 
 def main():
     parser = argparse.ArgumentParser(prog="macd 0.0", description="MCAD from CMD",
@@ -24,8 +26,13 @@ def main():
     parser.add_argument('-e','--end',type=validate_date, required=True, help="End date")
 
     args = parser.parse_args()
+    print(args.start)
+    if str(args.start) >= str(args.end):
+        parser.error(Fore.RED + Style.BRIGHT + "Start date must be minor than end date" + Fore.RESET +Style.RESET_ALL)
+        
 
 if __name__ == "__main__":
     main()
+    
     
     
