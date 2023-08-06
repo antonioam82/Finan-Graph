bace59a7a74e33e8bd11ba8934013bcfb463eaf4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-08-06 17:01:43 +0200

    Update macd.py

--- macd.py
+++ macd.py
@@ -33,7 +33,57 @@ def get_data(s,st,e):
         plt.plot(df.Senal, color="red")
         plt.plot(df.MACD)
         Buy, Sell = [], []
-  
+        
+        plt.subplot(2,1,2)
+        plt.plot(df.Senal, color="red")
+        plt.plot(df.MACD)
+        Buy, Sell = [], []
+        for i in range(2, len(df)):
+            if df.MACD.iloc[i] > df.Senal.iloc[i] and df.MACD.iloc[i-1] < df.Senal.iloc[i-1]:
+                Buy.append(i)
+            elif df.MACD.iloc[i] < df.Senal.iloc[i] and df.MACD.iloc[i-1] > df.Senal.iloc[i-1]:
+                Sell.append(i)
+
+        print("Dates sales:")
+        print(df.iloc[Sell].index, df.iloc[Sell].Close)
+        sellinfo = df.iloc[Sell].index, df.iloc[Sell].Close
+        print("Bullish dates:")
+        print(df.iloc[Buy].index, df.iloc[Buy].Close)
+        buyinfo = df.iloc[Buy].index, df.iloc[Buy].Close
+
+        #Get data from bullish
+        df1 = pd.DataFrame.from_records(buyinfo)
+        print(df1)
+        #Dataframe to dict:
+        diccionario = df1.to_dict()
+        #counting the data for get the last date for buy:
+        keys = list(diccionario.keys())
+        contarkeys = keys[-1]
+        print("count:",contarkeys)
+        print("Last info for buy:")
+        print(diccionario[contarkeys])
+        print("Last date for buy:")
+        date1 = diccionario[contarkeys][0]
+        dateforbuy = date1.strftime("%d/%m/%Y")
+        print(dateforbuy)
+        print("########")
+        print("Today date:")
+        now = datetime.now()
+        datetoday = now.strftime("%d/%m/%Y")
+        print(datetoday)
+        print("Yesterday date:")
+        yesterday = np.datetime64('today', 'D') - np.timedelta64(1, 'D')
+        print(yesterday)
+
+        print("Final decision:")
+        if str(dateforbuy) == str(yesterday):
+            print("MACD = BUY")
+        else:
+            print("Not news")
+
+
+        
+        
     except Exception as e:
         print(Fore.RED + Style.BRIGHT + str(e) + Fore.RESET +Style.RESET_ALL )
 
@@ -54,3 +104,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
