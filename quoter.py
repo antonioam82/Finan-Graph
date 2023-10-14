477e52824961f4b506ba5e01bed8ecb767b2777e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-10-14 16:06:06 +0200

    Update quoter.py

--- quoter.py
+++ quoter.py
@@ -7,7 +7,8 @@ from colorama import init, Fore, Back, Style
 def main():
     parser = argparse.ArgumentParser(prog="QUOTER 0.0",description="Show quotes in real time")
     parser.add_argument('-tick', '--ticker', required=True, type=str, help='Ticker name')
-    parser.add_argument('-int', '--interval', type=str, help='Interval')
+    parser.add_argument('-int', '--interval', type=str,default='1min',
+                        choices=['1min', '5min', '15min', '30min', '1hour', '1day'], help='Interval')
 
     args = parser.parse_args()
     print("OK")
