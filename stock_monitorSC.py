681bbc02a564e92b466a29afea458617f0882bae
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-01-17 15:48:34 +0100

    Update stock_monitorSC.py

--- stock_monitorSC.py
+++ stock_monitorSC.py
@@ -3,11 +3,12 @@
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from matplotlib import style
 import numpy as np
 
-# Función principal
 def main():
     # Crear la ventana principal
+    style.use('dark_background')
     root = tk.Tk()
     root.title("Stock Screen")
     root.geometry("730x500")
@@ -16,12 +17,10 @@ def main():
     plot = fig.add_subplot(1, 1, 1)
     plot.grid()
 
-    # Incorporar la figura en la interfaz de Tkinter
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas_widget = canvas.get_tk_widget()
     canvas_widget.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
-    # Iniciar el bucle principal de Tkinter
     root.mainloop()
 
 if __name__ == "__main__":
